c57dd1577f661c6626938008d506e3f08fd522f7
enochndika <enochbeloved@gmail.com>
2021-07-20 16:10:34 +0100

    change page title according to node env

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Salvia-kit Dashboard v4 Next.js</title>
+        <title>
+          {process.env.NODE_ENV === 'development'
+            ? 'Salvia-kit Dashboard v4 Next.js'
+            : 'Salvia-kit Dashboard v4 Demo'}
+        </title>
       </Head>
       <DashboardLayout>
         <Component {...pageProps} />
