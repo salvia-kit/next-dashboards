31d98c7aa8da8244b902fa4a5061b84be5461918
enochndika <enochbeloved@gmail.com>
2021-07-28 00:21:48 +0100

    add initial value for useRef, compare state value with previous value && update title

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>
-          {process.env.NODE_ENV === 'development'
-            ? 'Salvia-kit Dashboard v4 Next.js'
-            : 'Salvia-kit Dashboard v4 Demo'}
-        </title>
+        <title>Salvia-kit Dashboard v4 Next.js</title>
       </Head>
       <DashboardLayout>
         <Component {...pageProps} />
--- src/dashboard/provider/context.js
+++ src/dashboard/provider/context.js
@@ -6,12 +6,12 @@ const Context = React.createContext({});
 
 export default function DashboardProvider({ children }) {
   const [open, setOpen] = React.useState(false);
-  const ref = React.useRef();
+  const ref = React.useRef(null);
   const router = useRouter();
 
   const toggle = React.useCallback(() => {
-    setOpen(!open);
-  }, [open]);
+    setOpen((prevState) => !prevState);
+  }, []);
 
   // set the html tag overflow to hidden
   // close side navigation when route starts changing
