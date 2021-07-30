e34b1be3bd71e2ef0b5ed017ccfc0e64b4dfbdad
enochndika <enochbeloved@gmail.com>
2021-07-30 04:47:46 +0100

    update sidenav header logo

--- src/dashboard/sidenavigation/header.js
+++ src/dashboard/sidenavigation/header.js
@@ -1,12 +1,7 @@
 export default function SidenavHeader() {
   return (
     <div className="bg-gray-900 flex items-center justify-center mb-6 pb-6 pt-3 sticky top-0 z-10">
-      <img
-        src="https://res.cloudinary.com/beloved/image/upload/v1623194875/Assets/en_znafdb.png"
-        width={80}
-        height={90}
-        alt="Enoch Ndika"
-      />
+      <img src="/images/2.png" width={80} height={90} alt="Enoch Ndika" />
     </div>
   );
 }
