ae196cd20e89da7d3f71dc0f864187cbe26bcadb
Enoch Ndika <enochbeloved@gmail.com>
2023-07-14 17:48:32 +0100

    remove original dashboard code and update read.me file

--- src/pages/admin/credits.js
+++
@@ -1,5 +0,0 @@
-import Content from '../../components/content';
-
-export default function CreditsPage() {
-  return <Content title="Credits" />;
-}
--- src/pages/admin/status.js
+++
@@ -1,5 +0,0 @@
-import Content from '../../components/content';
-
-export default function StatusPage() {
-  return <Content title="Status" />;
-}
