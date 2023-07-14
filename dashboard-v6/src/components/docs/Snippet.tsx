eed4979ca832469e5e60b002462186f20b11aecf
Enoch Ndika <enochbeloved@gmail.com>
2023-07-14 17:59:55 +0100

    add dashboard v6

---
+++ dashboard-v6/src/components/docs/Snippet.tsx
@@ -0,0 +1,19 @@
+export function Snippet() {
+  return (
+    <pre className="max-w-full overflow-x-auto rounded border bg-gray-100 px-4 py-1 font-mono text-sm text-black">
+      {`[ 
+  {
+    title: "Users",
+    icon: <HomeIcon />,
+    link: "/",
+  },
+  {
+    title: "Medias",
+    icon: <MediaIcon />,
+    link: "/admin/medias",
+  },
+]
+`}
+    </pre>
+  );
+}
---
+++ dashboard-v6/src/dashboard/sidebar/icons/ServerIcon.tsx
@@ -0,0 +1,16 @@
+export function ServerIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
