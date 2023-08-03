a7ed264a50d0491ec745e1d313f871fb3dd39e90
Enoch Ndika <enochbeloved@gmail.com>
2023-08-03 14:38:09 +0100

    fix eslint for dashboard-v2

--- dashboard-v2/src/dashboard/Overlay.tsx
+++ dashboard-v2/src/dashboard/Overlay.tsx
@@ -1,7 +1,7 @@
 import { useDashboardContext } from "./Provider";
 
 const style = {
-  overlay: `bg-black fixed h-screen left-0 opacity-40 top-0 w-screen z-30 lg:hidden`,
+  overlay: "bg-black fixed h-screen left-0 opacity-40 top-0 w-screen z-30 lg:hidden",
 };
 
 // The overlay will only be visible on small screens to emphasize the focus on the side navigation when it is open.
